151229ce471033e0fd1dae73d075b4c201017ad2
achyuthdasari <achyuthdasari.24@gmail.com>
2021-04-18 17:51:49 +0530

    json updated

--- quizajax.js
+++ quizajax.js
@@ -1,5 +1,5 @@
 var request = new XMLHttpRequest();
-request.open('GET','C:\Users\Achyuth\Desktop\MSIT_requirements\React\Week_2\Day_10\Quiz_Ajax\Questions.txt')
+request.open('GET','https://achyuthdasari.github.io/Quiz_Ajax/Questions.txt')
 var data = JSON.parse(request.response);
 function display(){
     console.log(data[0])
